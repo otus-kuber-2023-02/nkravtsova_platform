b4d48467da08052ceb8ea5a9b469608733546bba
Nataly <nkravtsova@solit-clouds.ru>
2023-05-05 11:16:34 +0400

    edit kubernetes-intro

---
+++ kubernetes-intro/web/app.js
@@ -0,0 +1,4 @@
+const connect = require('connect');
+const serveStatic = require('serve-static');
+
+connect().use(serveStatic(__dirname)).listen(8000, () => console.log('Server running on port 8000'))
